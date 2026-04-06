3de2be95b0d9ad2e0ccc94661fa2bda0f3f6a372
Yennapusa kavya <yennapusakavya0201@gmail.com>
2026-04-06 16:20:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,12 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-#Title
-st.title("📊 Streamlit Demo App")
+st.title("Chess Box Color")
 
-#Text
-st.write("This is a simple Streamlit app deployed on Streamlit Cloud.")
+row = st.number_input("Enter row (1-8)", 1, 8)
+col = st.number_input("Enter column (1-8)", 1, 8)
 
-#User input
-name = st.text_input("Enter your name")
-
-if name:
-    st.success(f"Hello {name}, welcome to Streamlit!")
-
-#Slider
-number = st.slider("Select a number", 1, 100)
-
-st.write("Square of the number is:", number**2)
-
-#Generate sample data
-data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns=["A", "B", "C"]
-)
-
-#Show dataframe
-st.subheader("Sample Data")
-st.dataframe(data)
-
-#Chart
-st.subheader("Line Chart")
-st.line_chart(data)
+if st.button("Check"):
+    if (row + col) % 2 == 0:
+        st.write("White Box")
+    else:
+        st.write("Black Box")
